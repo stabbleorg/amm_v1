154cb21893ee699dba4ff75597cfdd6aad6c3fbf
stabble-devops <keisuke@stabble.org>
2023-11-28 23:26:56 -0500

    chore: bump dependency versions

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -11,7 +11,7 @@ import { setupStablePoolProgram } from "./pool-stable";
 import { parseKeypair } from "./utils";
 
 program
-  .version("0.3.0")
+  .version("0.3.1")
   .option("-k, --keypair <path>", "wallet keypair", parseKeypair)
   .option("-u, --url <string>", "RPC monk or url", "devnet")
   .option("-s, --simulate", "simulate transaction")
