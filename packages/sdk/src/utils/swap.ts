d7804b4144d1bfcd60e614fae60afd6192184363
stabble-devops <keisuke@stabble.org>
2024-05-20 10:03:24 -0400

    chore: support direct_route_only for SOR

--- packages/sdk/src/utils/swap.ts
+++ packages/sdk/src/utils/swap.ts
@@ -1,8 +1,8 @@
+import { NATIVE_MINT } from "@solana/spl-token";
 import { Keypair, PublicKey, Signer, TransactionInstruction, TransactionSignature } from "@solana/web3.js";
 import { FloatLike, TransactionArgs } from "@stabbleorg/anchor-contrib";
 import { Pool, StablePool, StablePoolData, WeightedPool, WeightedPoolData } from "../accounts";
 import { StableSwapContext, WeightedSwapContext } from "../programs";
-import { NATIVE_MINT } from "@solana/spl-token";
 
 export type BatchSwapRoute = {
   pool: Pool<StablePoolData | WeightedPoolData>;
@@ -256,16 +256,225 @@ export class Swap {
     }
   }
 
+  static async multiHopInstructions({
+    weightedSwap,
+    stableSwap,
+    routes,
+    amountIn,
+    minimumAmountOut,
+  }: TransactionArgs<{
+    weightedSwap: WeightedSwapContext;
+    stableSwap: StableSwapContext;
+    routes: BatchSwapRoute[];
+    amountIn: FloatLike;
+    minimumAmountOut: FloatLike;
+  }>): Promise<TransactionInstruction[]> {
+    if (!weightedSwap.walletAddress.equals(stableSwap.walletAddress))
+      throw Error("Singers mismatched for weighted and stable swap contexts.");
+
+    // direct swap
+    if (routes.length === 1) {
+      const args: SwapInstructionArgs = {
+        pool: routes[0].pool,
+        mintInAddress: routes[0].mintInAddress,
+        mintOutAddress: routes[0].mintOutAddress,
+        amountIn,
+        minimumAmountOut,
+      };
+
+      if (routes[0].pool instanceof StablePool) {
+        return stableSwap.swapInstructions(args);
+      } else if (routes[0].pool instanceof WeightedPool) {
+        return weightedSwap.swapInstructions(args);
+      } else {
+        throw Error("Pool not supported");
+      }
+    }
+    // 2-hop swap
+    else if (routes.length === 2) {
+      const signers: Signer[] = [];
+      const instructions: TransactionInstruction[] = [];
+
+      let tokenInAddress: PublicKey | undefined = undefined;
+      if (routes[0].mintInAddress.equals(NATIVE_MINT)) {
+        const keypair = Keypair.generate();
+        signers.push(keypair);
+        tokenInAddress = keypair.publicKey;
+        instructions.push(...(await weightedSwap.transferWSOLInstructions(tokenInAddress, amountIn)));
+      }
+
+      let tokenOutAddress: PublicKey | undefined = undefined;
+      {
+        const keypair = Keypair.generate();
+        signers.push(keypair);
+        tokenOutAddress = keypair.publicKey;
+        instructions.push(
+          ...(await weightedSwap.createIntermediateTokenAccountInstructions(tokenOutAddress, routes[0].mintOutAddress)),
+        );
+      }
+
+      const args0: SwapInstructionArgs = {
+        pool: routes[0].pool,
+        mintInAddress: routes[0].mintInAddress,
+        mintOutAddress: routes[0].mintOutAddress,
+        amountIn,
+        tokenInAddress,
+        tokenOutAddress,
+      };
+
+      if (routes[0].pool instanceof WeightedPool) {
+        instructions.push(...(await weightedSwap.swapInstructions(args0)));
+      } else if (routes[0].pool instanceof StablePool) {
+        instructions.push(...(await stableSwap.swapInstructions(args0)));
+      } else {
+        throw Error("Pool not supported");
+      }
+
+      tokenInAddress = tokenOutAddress;
+      if (routes[1].mintOutAddress.equals(NATIVE_MINT)) {
+        const keypair = Keypair.generate();
+        signers.push(keypair);
+        tokenOutAddress = keypair.publicKey;
+        instructions.push(
+          ...(await weightedSwap.createIntermediateTokenAccountInstructions(tokenOutAddress, routes[1].mintOutAddress)),
+        );
+      } else {
+        tokenOutAddress = undefined;
+      }
+
+      const args1: SwapInstructionArgs = {
+        pool: routes[1].pool,
+        mintInAddress: routes[1].mintInAddress,
+        mintOutAddress: routes[1].mintOutAddress,
+        minimumAmountOut,
+        tokenInAddress,
+        tokenOutAddress,
+      };
+
+      if (routes[1].pool instanceof WeightedPool) {
+        instructions.push(...(await weightedSwap.swapInstructions(args1)));
+      } else if (routes[1].pool instanceof StablePool) {
+        instructions.push(...(await stableSwap.swapInstructions(args1)));
+      } else {
+        throw Error("Pool not supported");
+      }
+
+      return instructions;
+    }
+    // 3-hop swap
+    else if (routes.length === 3) {
+      const signers: Signer[] = [];
+      const instructions: TransactionInstruction[] = [];
+
+      let tokenInAddress: PublicKey | undefined = undefined;
+      if (routes[0].mintInAddress.equals(NATIVE_MINT)) {
+        const keypair = Keypair.generate();
+        signers.push(keypair);
+        tokenInAddress = keypair.publicKey;
+        instructions.push(...(await weightedSwap.transferWSOLInstructions(tokenInAddress, amountIn)));
+      }
+
+      let tokenOutAddress: PublicKey | undefined = undefined;
+      {
+        const keypair = Keypair.generate();
+        signers.push(keypair);
+        tokenOutAddress = keypair.publicKey;
+        instructions.push(
+          ...(await weightedSwap.createIntermediateTokenAccountInstructions(tokenOutAddress, routes[0].mintOutAddress)),
+        );
+      }
+
+      const args0: SwapInstructionArgs = {
+        pool: routes[0].pool,
+        mintInAddress: routes[0].mintInAddress,
+        mintOutAddress: routes[0].mintOutAddress,
+        amountIn,
+        tokenInAddress,
+        tokenOutAddress,
+      };
+
+      if (routes[0].pool instanceof WeightedPool) {
+        instructions.push(...(await weightedSwap.swapInstructions(args0)));
+      } else if (routes[0].pool instanceof StablePool) {
+        instructions.push(...(await stableSwap.swapInstructions(args0)));
+      } else {
+        throw Error("Pool not supported");
+      }
+
+      tokenInAddress = tokenOutAddress;
+      {
+        const keypair = Keypair.generate();
+        signers.push(keypair);
+        tokenOutAddress = keypair.publicKey;
+        instructions.push(
+          ...(await weightedSwap.createIntermediateTokenAccountInstructions(tokenOutAddress, routes[1].mintOutAddress)),
+        );
+      }
+
+      const args1: SwapInstructionArgs = {
+        pool: routes[1].pool,
+        mintInAddress: routes[1].mintInAddress,
+        mintOutAddress: routes[1].mintOutAddress,
+        tokenInAddress,
+        tokenOutAddress,
+      };
+
+      if (routes[1].pool instanceof WeightedPool) {
+        instructions.push(...(await weightedSwap.swapInstructions(args1)));
+      } else if (routes[1].pool instanceof StablePool) {
+        instructions.push(...(await stableSwap.swapInstructions(args1)));
+      } else {
+        throw Error("Pool not supported");
+      }
+
+      tokenInAddress = tokenOutAddress;
+      if (routes[2].mintOutAddress.equals(NATIVE_MINT)) {
+        const keypair = Keypair.generate();
+        signers.push(keypair);
+        tokenOutAddress = keypair.publicKey;
+        instructions.push(
+          ...(await weightedSwap.createIntermediateTokenAccountInstructions(tokenOutAddress, routes[2].mintOutAddress)),
+        );
+      } else {
+        tokenOutAddress = undefined;
+      }
+
+      const args2: SwapInstructionArgs = {
+        pool: routes[2].pool,
+        mintInAddress: routes[2].mintInAddress,
+        mintOutAddress: routes[2].mintOutAddress,
+        minimumAmountOut,
+        tokenInAddress,
+        tokenOutAddress,
+      };
+      if (routes[2].pool instanceof WeightedPool) {
+        instructions.push(...(await weightedSwap.swapInstructions(args2)));
+      } else if (routes[2].pool instanceof StablePool) {
+        instructions.push(...(await stableSwap.swapInstructions(args2)));
+      } else {
+        throw Error("Pool not supported");
+      }
+
+      return instructions;
+    }
+    // 4-hop swap
+    else {
+      throw Error("It only supports up to 3-hop swap");
+    }
+  }
+
   static searchRoutes({
     pools,
     mintInAddress,
     mintOutAddress,
     amountIn,
+    directRoutesOnly = false,
   }: {
     pools: Pool<StablePoolData | WeightedPoolData>[];
     mintInAddress: PublicKey;
     mintOutAddress: PublicKey;
     amountIn: number;
+    directRoutesOnly?: boolean;
   }): { routes: BatchSwapRoute[]; amountOut: number } {
     let routes: BatchSwapRoute[] = [];
     let amountOut = 0;
@@ -292,52 +501,57 @@ export class Swap {
         },
       ];
       amountOut = pool.getSwapAmountOut(mintInAddress, mintOutAddress, amountIn);
+      console.debug("R1:", amountOut);
     }
 
-    // R1 routes
-    const pools_R1 = pools
-      .filter((p) => pools_R0.map((r0) => !p.address.equals(r0.address)))
-      .filter((p) => p.tokens.some((token) => token.mintAddress.equals(mintInAddress)));
+    if (!directRoutesOnly) {
+      // R1 routes
+      const pools_R1 = pools
+        .filter((p) => pools_R0.map((r0) => !p.address.equals(r0.address)))
+        .filter((p) => p.tokens.some((token) => token.mintAddress.equals(mintInAddress)));
 
-    // R2 routes
-    const pools_R2 = pools
-      .filter((p) => pools_R0.map((r0) => !p.address.equals(r0.address)))
-      .filter((p) => pools_R1.map((r1) => !p.address.equals(r1.address)));
+      // R2 routes
+      const pools_R2 = pools
+        .filter((p) => pools_R0.map((r0) => !p.address.equals(r0.address)))
+        .filter((p) => pools_R1.map((r1) => !p.address.equals(r1.address)));
 
-    for (const r1 of pools_R1) {
-      for (const t1 of r1.tokens.filter((t) => !t.mintAddress.equals(mintInAddress))) {
-        const amountOut_R1 = r1.getSwapAmountOut(mintInAddress, t1.mintAddress, amountIn);
+      for (const r1 of pools_R1) {
+        for (const t1 of r1.tokens.filter((t) => !t.mintAddress.equals(mintInAddress))) {
+          const amountOut_R1 = r1.getSwapAmountOut(mintInAddress, t1.mintAddress, amountIn);
 
-        for (const r2 of pools_R2) {
-          for (const t2 of r2.tokens.filter((t) => !t.mintAddress.equals(t1.mintAddress))) {
-            const amountOut_R2 = r2.getSwapAmountOut(t1.mintAddress, t2.mintAddress, amountOut_R1);
+          for (const r2 of pools_R2) {
+            for (const t2 of r2.tokens.filter((t) => !t.mintAddress.equals(t1.mintAddress))) {
+              const amountOut_R2 = r2.getSwapAmountOut(t1.mintAddress, t2.mintAddress, amountOut_R1);
 
-            if (t2.mintAddress.equals(mintOutAddress)) {
-              if (amountOut_R2 > amountOut) {
-                routes = [
-                  { pool: r1, mintInAddress, mintOutAddress: t1.mintAddress },
-                  { pool: r2, mintInAddress: t1.mintAddress, mintOutAddress },
-                ];
-                amountOut = amountOut_R2;
-              }
-            } else {
-              // R3 routes
-              const pools_R3 = pools_R2
-                .filter((p) => !p.address.equals(r2.address))
-                .filter(
-                  (p) =>
-                    p.tokens.some((t) => t.mintAddress.equals(t2.mintAddress)) &&
-                    p.tokens.some((t) => t.mintAddress.equals(mintOutAddress)),
-                );
-              for (const r3 of pools_R3) {
-                const amountOut_R3 = r3.getSwapAmountOut(t2.mintAddress, mintOutAddress, amountOut_R2);
-                if (amountOut_R3 > amountOut) {
+              if (t2.mintAddress.equals(mintOutAddress)) {
+                if (amountOut_R2 > amountOut) {
                   routes = [
                     { pool: r1, mintInAddress, mintOutAddress: t1.mintAddress },
-                    { pool: r2, mintInAddress: t1.mintAddress, mintOutAddress: t2.mintAddress },
-                    { pool: r3, mintInAddress: t2.mintAddress, mintOutAddress },
+                    { pool: r2, mintInAddress: t1.mintAddress, mintOutAddress },
                   ];
-                  amountOut = amountOut_R3;
+                  amountOut = amountOut_R2;
+                  console.debug("R2:", amountOut);
+                }
+              } else {
+                // R3 routes
+                const pools_R3 = pools_R2
+                  .filter((p) => !p.address.equals(r2.address))
+                  .filter(
+                    (p) =>
+                      p.tokens.some((t) => t.mintAddress.equals(t2.mintAddress)) &&
+                      p.tokens.some((t) => t.mintAddress.equals(mintOutAddress)),
+                  );
+                for (const r3 of pools_R3) {
+                  const amountOut_R3 = r3.getSwapAmountOut(t2.mintAddress, mintOutAddress, amountOut_R2);
+                  if (amountOut_R3 > amountOut) {
+                    routes = [
+                      { pool: r1, mintInAddress, mintOutAddress: t1.mintAddress },
+                      { pool: r2, mintInAddress: t1.mintAddress, mintOutAddress: t2.mintAddress },
+                      { pool: r3, mintInAddress: t2.mintAddress, mintOutAddress },
+                    ];
+                    amountOut = amountOut_R3;
+                    console.debug("R3:", amountOut);
+                  }
                 }
               }
             }
